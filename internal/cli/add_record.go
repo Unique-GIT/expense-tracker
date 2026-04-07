f50ba07b79594a1771452fcc1a091b533995f878
Aakash <aakash04unique@gmail.com>
2026-04-08 01:39:16 +0530

    reshuffling of main file

--- internal/cli/add_record.go
+++ internal/cli/add_record.go
@@ -3,11 +3,11 @@ package cli
 func AddRecord(object string, cost float32, label string, user string) error {
 	// Later some state config will call this
 
-	// Check if label exists or not
-	// If not return error to create it
-
 	// Check the user-id
 	// If not return error to say unauthorized access
+
+	// Check if label exists or not
+	// If not return error to create it
 
 	// If yes then proceed
 
