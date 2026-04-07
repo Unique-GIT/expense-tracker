9dd6bf566a53bda6fd34dc05a27369bca6077b20
Aakash <aakash04unique@gmail.com>
2026-04-08 01:34:27 +0530

    added boilerplate code for adding transaction

---
+++ internal/cli/add_record.go
@@ -0,0 +1,15 @@
+package cli
+
+func AddRecord(object string, cost float32, label string, user string) error {
+	// Later some state config will call this
+
+	// Check if label exists or not
+	// If not return error to create it
+
+	// Check the user-id
+	// If not return error to say unauthorized access
+
+	// If yes then proceed
+
+	return nil
+}
